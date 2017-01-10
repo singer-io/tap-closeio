aa482bea59b74f3daa696b97d49aa37fd43e9d72
Mike DeLaurentis <miked@stitchdata.com>
2017-01-10 16:56:37 -0500

    Working on stuff

--- stream_closeio.py
+++ stream_closeio.py
@@ -13,11 +13,15 @@ import backoff
 logger = logging.getLogger()
 
 base_url = 'https://app.close.io/api/v1'
-return_limit = 100
 
-auth = (os.environ['STITCH_CLOSEIO_API_KEY'],'')
-print(auth)
+return_limit = 100
+default_start_date = '2000-01-01T00:00:00Z'
+
 session = requests.Session()
+
+state = {
+    'leads': default_start_date
+}
 
 class StitchException(Exception):
     def __init__(self, message):
@@ -48,34 +52,94 @@ def request(**kwargs):
     response.raise_for_status()
     return response
 
-def get_leads(start_date, offset=0):
+def get_leads(auth, query_date, offset=0):
+    global state
+
     params = {
         '_limit': return_limit,
         '_skip': offset,
-        'query': 'date_updated >= ' + start_date + ' sort:date_updated'
+        'query': 'date_updated >= ' + query_date + ' sort:date_updated'
     }
-    
+    logger.info("Fetching leads starting at " + query_date + "; offset " + str(offset) + "; limit " + str(return_limit));
     response = request(url=base_url + '/lead/', params=params, auth=auth)
 
-    print(response.json())
+    body = response.json()
+    data = body['data']
+    if len(data) == 0:
+        return
     
-def main():
-    parser = argparse.ArgumentParser(prog='Close.io Streamer')
-    args = parser.parse_args()
+    ss.write_records('leads', data)
+    state['leads'] = data[-1]['date_updated']
+    ss.write_bookmark(state)
+    
+    if 'has_more' in body and body['has_more']:
+        get_leads(auth, query_date, offset = offset + return_limit)
 
-    configure_logging()
+def do_check(args):
+    with open(args.config) as file:
+        config = json.load(file)
+
+    auth = (config['api_key'],'')
+
+    params = {
+        '_limit': 10
+    }
+
+    try:
+        request(url=base_url + '/lead/', params=params, auth=auth)
+    except requests.exceptions.RequestException as e:
+        logger.fatal("Error checking connection using " + e.request.url +
+                     "; received status " + str(e.response.status_code) +
+                     ": " + e.response.text)
+        sys.exit(-1)
+
+def do_sync(args):
+    global state
+    with open(args.config) as file:
+        config = json.load(file)
+
+    if args.state != None:
+        with open(args.state) as file:
+            state = json.load(file)
 
     logger.info('Replicating all Close.io data')
 
     ## TODO: write schemas to stream
 
+    auth = (config['api_key'],'')
+    
     try:
-        get_leads('2017-01-01T00:00:00Z')
+        get_leads(auth, state['leads'])
     except requests.exceptions.RequestException as e:
         logger.fatal("Error on " + e.request.url +
                      "; received status " + str(e.response.status_code) +
                      ": " + e.response.text)
         sys.exit(-1)
     
+def main():
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers()
+    
+    parser_check = subparsers.add_parser('check')
+    parser_check.set_defaults(func=do_check)
+
+    parser_sync = subparsers.add_parser('sync')
+    parser_sync.set_defaults(func=do_sync)
+
+    for subparser in [parser_check, parser_sync]:
+        subparser.add_argument('-c', '--config', help='Config file', required=True)    
+        subparser.add_argument('-s', '--state', help='State file')
+    
+    args = parser.parse_args()
+    configure_logging()
+
+    if 'func' in args:
+        args.func(args)
+    else:
+        parser.print_help()
+        exit(1)
+
+
 if __name__ == '__main__':
     main()
