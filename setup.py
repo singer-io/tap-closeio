bce33ab642e74600105f14443bb3ac1d82018ab9
Mike DeLaurentis <miked@stitchdata.com>
2017-02-02 15:01:13 -0500

    Version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-closeio',
-      version='0.1.2',
+      version='0.1.1',
       description='Taps CloseIO data',
       author='Stitch',
       url='https://github.com/stitchstreams/tap-closeio',
