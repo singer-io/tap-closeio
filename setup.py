535bb03b63f280cbc02a24e463a592dc8a456031
John Miller <jmiller@rjmetrics.com>
2017-02-28 14:01:46 -0500

    convert to freshdesk style tap

--- setup.py
+++ setup.py
@@ -3,28 +3,28 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-closeio',
-      version='0.3.2',
+      version='0.4.0',
       description='Singer.io tap for extracting data from the CloseIO API',
       author='Stitch',
       url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_closeio'],
-      install_requires=['stitchclient>=0.4.1',
-                        'singer-python>=0.1.0',
-                        'requests==2.12.4',
-                        'arrow==0.10.0',
-                        'six==1.10.0',
-                        'backoff==1.3.2',
-                        'python-dateutil==2.6.0'],
+      install_requires=[
+          'singer-python>=0.1.0',
+          'requests==2.12.4',
+          'backoff==1.3.2',
+          'pendulum==1.1.0',
+      ],
       entry_points='''
           [console_scripts]
           tap-closeio=tap_closeio:main
       ''',
       packages=['tap_closeio'],
       package_data = {
-          'tap_closeio': [
+          'tap_closeio/schemas': [
               'leads.json',
               'activities.json'
-              ]
-          }
+          ]
+      },
+      include_package_data=True,
 )
