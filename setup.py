2590660886275c80b320d1d1d558814999c9e908
Richard Clark <rclark@rjmetrics.com>
2017-06-29 19:17:27 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-closeio',
-      version='0.6.5',
+      version='0.6.6',
       description='Singer.io tap for extracting data from the CloseIO API',
       author='Stitch',
       url='http://singer.io',
