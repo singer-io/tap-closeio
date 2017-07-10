39be98d7bef16fd2da268c37243fa86ff519f444
Chris Capurso <chrisc@stitchdata.com>
2017-07-10 14:08:23 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-closeio',
-      version='0.6.7',
+      version='0.6.8',
       description='Singer.io tap for extracting data from the CloseIO API',
       author='Stitch',
       url='http://singer.io',
