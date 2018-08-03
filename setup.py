de015639ee3f532c18cad3e7dfe55f2add6dc70f
Tim Visher <tim@stitchdata.com>
2018-08-03 17:36:03 +0000

    singer-python 5.2.0

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-closeio',
-      version='1.0.1',
+      version='1.0.2',
       description='Singer.io tap for extracting data from the CloseIO API',
       author='Stitch',
       url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_closeio'],
       install_requires=[
-          'singer-python==5.2.1',
+          'singer-python==5.2.0',
           'pendulum==1.2.0',
           'requests==2.12.4',
       ],
