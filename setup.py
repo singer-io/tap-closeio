3ef926e880106c4021ec5b64ae6cdd5913181bb7
Tim Visher <tim@stitchdata.com>
2018-08-29 16:01:29 +0000

    1.4.0 → 1.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-closeio',
-      version='1.4.0',
+      version='1.5.0',
       description='Singer.io tap for extracting data from the CloseIO API',
       author='Stitch',
       url='http://singer.io',
